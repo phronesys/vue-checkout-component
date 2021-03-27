38001209b8c19f6bf2f0bb14c2316ac7d35d3802
phronesys <daniel.dazarola@gmail.com>
2021-03-27 18:34:24 -0300

    CartList test, removing magic numbers to improve my test xD

--- tests/unit/checkout/cart/CartList.spec.js
+++ tests/unit/checkout/cart/CartList.spec.js
@@ -1,10 +1,43 @@
-import {shallowMount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import CartList from '@/components/checkout/cart/CartList';
 import CartItem from '@/components/checkout/cart/CartItem';
 
+const SILLA_GAMER_POS = 0;
+const PILLOWS_POS = 1;
+const REMOVE = -1;
+const ADD = 1;
+
+let wrapper = null;
+
+beforeEach(() => {
+  wrapper = shallowMount(CartList);
+})
+afterEach(() => {
+  wrapper.unmount();
+})
 describe('CartList', () => {
   it('renders CartItem component', () => {
-    const wrapper = shallowMount(CartList);
     expect(wrapper.findComponent(CartItem).exists()).toBe(true);
+  })
+  //sends total price of all items on the cart to the parent component
+  it('updateCounters change the counter of the given id', () => {
+    const wrapper = shallowMount(CartList);
+    wrapper.vm.updateCounters(REMOVE, SILLA_GAMER_POS);
+    expect(wrapper.vm.items[0].counter).toEqual(0); // no chairs
+    wrapper.vm.updateCounters(ADD, SILLA_GAMER_POS);
+    wrapper.vm.updateCounters(ADD, SILLA_GAMER_POS);
+    expect(wrapper.vm.items[0].counter).toEqual(2); // two chairs
+
+    wrapper.vm.updateCounters(ADD, PILLOWS_POS);
+    wrapper.vm.updateCounters(ADD, PILLOWS_POS);
+    wrapper.vm.updateCounters(ADD, PILLOWS_POS);
+    expect(wrapper.vm.items[1].counter).toEqual(4);
+  })
+  it('after updateCounters emits the new total price', () => {
+    const CHAIR_OFFER_PRICE = 129;
+    const PILLOWS_OFFER_PRICE = 5;
+    const SHIPPING = 6;
+    const TOTAL_DEFAULT = CHAIR_OFFER_PRICE + PILLOWS_OFFER_PRICE + SHIPPING;
+    expect(wrapper.vm.totalPrice).toEqual(TOTAL_DEFAULT);
   })
 })
