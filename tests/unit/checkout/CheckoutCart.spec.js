95c68ae20af91b7507c10eb810635c4cbc8a00c3
phronesys <daniel.dazarola@gmail.com>
2021-03-27 22:00:07 -0300

    I think is the last cart test, logic practically done now

--- tests/unit/checkout/CheckoutCart.spec.js
+++ tests/unit/checkout/CheckoutCart.spec.js
@@ -9,4 +9,11 @@ describe('CheckoutCart', () => {
     expect(wrapper.findComponent(CartList).exists()).toBe(true);
     expect(wrapper.findComponent(CartTotal).exists()).toBe(true);
   })
+  it('updates total calling updateTotal', () => {
+    const wrapper = shallowMount(CheckoutCart);
+    expect(wrapper.vm.totalPrice).toEqual(0);
+    const ANY_TOTAL = 100;
+    wrapper.vm.updateTotal(ANY_TOTAL);
+    expect(wrapper.vm.totalPrice).toEqual(100);
+  })
 })
