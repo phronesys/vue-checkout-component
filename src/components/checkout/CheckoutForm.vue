4b2d515e3f69917c6040f9f7d4ce0e78090249b2
phronesys <daniel.dazarola@gmail.com>
2021-04-07 23:22:16 -0400

    feat: checkout form

--- src/components/checkout/CheckoutForm.vue
+++ src/components/checkout/CheckoutForm.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>checkout form</div>
+  <form class="form" action="#">
+    <div class="form-titles">Contact information</div>
+    <form-input
+      placeholder="Enter your email..."
+      icon="email"
+      label="E-mail"
+    ></form-input>
+    <form-input
+      placeholder="Enter your phone..."
+      icon="phone"
+      label="Phone"
+    ></form-input>
+    <div class="my-10"></div>
+    <div class="form-titles">Shipping address</div>
+    <form-input
+      placeholder="Your name.."
+      icon="account_circle"
+      label="Full name"
+    ></form-input>
+    <form-input
+      placeholder="Your address.."
+      icon="home"
+      label="Address"
+    ></form-input>
+    <form-input
+      placeholder="Your city.."
+      icon="location_city"
+      label="City"
+    ></form-input>
+  </form>
 </template>
 
 <script>
+import FormInput from "./form/FormInput.vue";
 export default {
-
-}
+  components: {
+    FormInput,
+  },
+};
 </script>
 
-<style>
-
+<style lang="postcss">
+.form {
+  width: 360px;
+  margin-top: 2rem;
+  margin-bottom: 4rem;
+  
+}
+.form-titles {
+  font-weight: 600;
+  font-size: 14px;
+  @apply text-left ml-4 mt-4 mb-2;
+}
 </style>
---
+++ src/components/checkout/form/FormInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="form-label">{{label}}</div>
+  <div class="flex flex-row items-center justify-center">
+    <label class="material-icons form-icon">{{ icon }}</label>
+    <input class="form-input" type="text" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    }
+  },
+};
+</script>
+
+<style lang="postcss">
+.form-label {
+  font-size: 0.6rem;
+  @apply text-left ml-4 mt-4 mb-1;
+  color: #4F4F4F;
+  font-weight: 600;
+}
+.form-icon {
+  font-size: 1rem;
+  color: #828282;
+  transform: translateX(1.6rem) scale(.75);
+}
+.form-input {
+  border: 1px solid #828282;
+  border-radius: 12px;
+  width: 110%;
+  height: 40px;
+  margin-left: -.5rem;
+  padding-left: 2.6rem;
+  color: #333;
+  font-weight: 600;
+  font-size: 12px;
+}
+.form-input::placeholder {
+  font-size: 0.75rem;
+  font-family: "Montserrat";
+  font-weight: 600;
+  color: #828282;
+}
+.form-input:focus {
+  outline: none;
+}
+</style>
